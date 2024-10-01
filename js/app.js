2b169f4aa1f39b306ef4f41f3e7a7369ef03919c
Daniele Simonetti <oppifjellet@gmail.com>
2024-10-01 19:37:10 +0200

    Implemented infrastructure for multiple issues

---
+++ js/app.js
@@ -0,0 +1,18 @@
+data = {
+    "issues": [
+        {
+            "title": "Uscita #1, Akoros - 24th Kalivet, 848",
+            "link": "/issues/issue001.html",
+            "excerpt": "<ul><li>Riapre la vecchia tratta commerciale</li></ul>"
+        }/*,
+        {
+            "title": "Uscita #1, Akoros - 24th Kalivet, 848",
+            "link": "/issues/issue001.html",
+            "excerpt": "<ul><li>Riapre la vecchia tratta commerciale</li></ul>"
+        }*/        
+    ]
+}
+
+document.addEventListener('alpine:init', () => {
+    Alpine.data('data', () => data)
+})
