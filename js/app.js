412fb6e0024723b9f575417f165ad51b2c95e1c2
Daniele Simonetti <oppifjellet@gmail.com>
2024-11-09 18:50:25 +0100

    Primo prop

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ data = {
             "title": "Uscita #1, Akoros - 24th Kalivet, 848",
             "link": "issues/issue001.html",
             "excerpt": "<ul><li>Riapre la vecchia tratta commerciale</li></ul>"
-        }/*,
+        },
         {
-            "title": "Uscita #1, Akoros - 24th Kalivet, 848",
-            "link": "/issues/issue001.html",
-            "excerpt": "<ul><li>Riapre la vecchia tratta commerciale</li></ul>"
-        }*/        
+            "title": "Lettera anonima",
+            "link": "/props/props001.html",
+            "excerpt": "<ul><li>Un offerta di lavoro</li></ul>"
+        }       
     ]
 }
 
