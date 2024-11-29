4ca216fdf93800e2d7b6835a3b769dabd1daa678
Daniele Simonetti <daniele.simonetti@aep-italia.it>
2024-11-29 15:01:07 +0100

    Add issue 002

--- js/app.js
+++ js/app.js
@@ -9,7 +9,12 @@ data = {
             "title": "Lettera anonima",
             "link": "props/props001.html",
             "excerpt": "<ul><li>Un offerta di lavoro</li></ul>"
-        }       
+        },
+        {
+            "title": "Uscita #2, Akoros - 31th Kalivet, 848",
+            "link": "issues/issue002.html",
+            "excerpt": "<ul><li>Processo in crisi</li></ul>"
+        }     
     ]
 }
 
